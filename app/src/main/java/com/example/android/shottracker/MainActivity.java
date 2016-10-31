85fa8a5d9ca206a4fff4b50cc419f63f8bf1c8b9
Vitnere <nemanjakolar@gmail.com>
2016-10-31 14:29:52 +0100

    layout build 1 - Setting up the Methods for basic display

--- app/src/main/java/com/example/android/shottracker/MainActivity.java
+++ app/src/main/java/com/example/android/shottracker/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.shottracker;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,4 +20,17 @@ public class MainActivity extends AppCompatActivity {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
+
+    public void display3(View v) {
+        displayForTeamA(3);
+    }
+
+    public void display2(View v) {
+        displayForTeamA(2);
+    }
+
+    public void display1(View v) {
+        displayForTeamA(1);
+    }
+
 }
