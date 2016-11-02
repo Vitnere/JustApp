f6a89b3539d4e467dc30f1454383c1381cd5ab00
Vitnere <nemanjakolar@gmail.com>
2016-11-02 15:46:12 +0100

    dinamic score update for team B, reset button

--- app/src/main/java/com/example/android/shottracker/MainActivity.java
+++ app/src/main/java/com/example/android/shottracker/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     int scoreTeamA = 0;
-
+    int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,27 +16,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    /**
-     * Displays the given score for Team A.
-     */
+    /*Team A*/
+
+    /*
+    * Display score for team A
+    * */
     public void displayForTeamA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        TextView scoreView = (TextView) findViewById(R.id.team_a_scoreA);
         scoreView.setText(String.valueOf(score));
     }
 
     /*
     * Increase score for three
     * */
-    public void display3(View v) {
+    public void display3A(View v) {
         scoreTeamA = scoreTeamA + 3;
         displayForTeamA(scoreTeamA);
-
     }
 
     /*
     * Increase score for two
     * */
-    public void display2(View v) {
+    public void display2A(View v) {
         scoreTeamA = scoreTeamA + 2;
         displayForTeamA(scoreTeamA);
     }
@@ -44,9 +45,52 @@ public class MainActivity extends AppCompatActivity {
     /*
     * Increase score for one
     * */
-    public void display1(View v) {
+    public void display1A(View v) {
         scoreTeamA = scoreTeamA + 1;
         displayForTeamA(scoreTeamA);
+    }
+
+    /*Team B*/
+
+    /*
+    * Display score for team B
+    * */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_scoreB);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /*
+   * Increase score for three
+   * */
+    public void display3B(View v) {
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /*
+    * Increase score for two
+    * */
+    public void display2B(View v) {
+        scoreTeamB = scoreTeamB + 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /*
+    * Increase score for one
+    * */
+    public void display1B(View v) {
+        scoreTeamB = scoreTeamB + 1;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /*RESET score to 0 BTN*/
+
+    public void reset(View v) {
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
     }
 
 }
